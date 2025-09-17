e994094ae3645614d0da20e9e317eb89f348829d
Ollie Joestar <bigzzzid@gmail.com>
2025-09-17 13:56:02 +0200

    preeval

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -16,15 +16,15 @@ int	main() {
 	std::cout << "Value at stringPTR:\t\t" << *stringPTR << std::endl;
 	std::cout << "Value at stringREF:\t\t" << stringREF << std::endl;
 
-	// std::cout << "----------" << std::endl;
-	// std::cout << "Changing the value of theString to 'NEW VALUE'" << std::endl;
-	// theString = "NEW VALUE";
-	//
-	// std::cout << "----------" << std::endl;
-	//
-	// std::cout << "Value of theString:\t\t" << theString << std::endl;
-	// std::cout << "Value at stringPTR:\t\t" << *stringPTR << std::endl;
-	// std::cout << "Value at stringREF:\t\t" << stringREF << std::endl;
+	std::cout << "----------" << std::endl;
+	std::cout << "Changing the value of theString to 'NEW VALUE'" << std::endl;
+	theString = "NEW VALUE";
+
+	std::cout << "----------" << std::endl;
+
+	std::cout << "Value of theString:\t\t" << theString << std::endl;
+	std::cout << "Value at stringPTR:\t\t" << *stringPTR << std::endl;
+	std::cout << "Value at stringREF:\t\t" << stringREF << std::endl;
 	
 	return 0;
 }
